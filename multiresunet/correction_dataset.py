fc3e1c7d2259d2afe33df2e0d6ce54fda6e0f9d0
dankydev <manicardi215@gmail.com>
2021-10-04 16:01:18 +0200

    Added MultiResUnet

---
+++ multiresunet/correction_dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+import os
+import cv2
+
+
+class WoodCorrectionDataset(Dataset):
+    def __init__(self, size=128, dataset_path='./dataset'):
+        pass
+    
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        # type: () -> int
+        return 0
