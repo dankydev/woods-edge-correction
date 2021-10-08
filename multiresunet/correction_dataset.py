4b8c27b775680228cf88d2a60665ed7bcfb9805b
dankydev <manicardi215@gmail.com>
2021-10-08 21:46:30 +0200

    Commit for merging

--- multiresunet/correction_dataset.py
+++ multiresunet/correction_dataset.py
@@ -5,11 +5,18 @@ import cv2
 
 class WoodCorrectionDataset(Dataset):
     def __init__(self, size=128, dataset_path='./dataset'):
-        pass
-    
+        self.images = []
+        images = [cv2.imread(os.path.join(dataset_path, x)) for x in os.listdir(dataset_path)
+                  if os.path.isfile(os.path.join(dataset_path, x))]
+        self.images = images
+
     def __getitem__(self, index):
-        pass
+        return None
 
     def __len__(self):
         # type: () -> int
-        return 0
+        return len(self.images)
+
+
+if __name__ == "__main__":
+    dataset = WoodCorrectionDataset()
