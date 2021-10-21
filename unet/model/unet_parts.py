6d7fb19464f401b5114ef13f105d10b14ae70782
dankydev <manicardi215@gmail.com>
2021-10-21 22:06:07 +0200

    Revert "Removed pooling layers, added depth to the network"
    This reverts commit c7945703ba6c7939399bff1136d04818509dec46.

--- unet/model/unet_parts.py
+++ unet/model/unet_parts.py
@@ -13,23 +13,17 @@ class DoubleConv(nn.Module):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
-
-        self.res_conv = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(5, 5)),
-            nn.BatchNorm2d(out_channels)
-        )
-
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
-        return self.res_conv(x) + self.double_conv(x)
+        return self.double_conv(x)
 
 
 class Down(nn.Module):
@@ -38,7 +32,7 @@ class Down(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
-            #nn.MaxPool2d(2),
+            nn.MaxPool2d(2),
             DoubleConv(in_channels, out_channels)
         )
 
@@ -49,30 +43,30 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, kernel_size=2, stride=2, padding=0, bilinear=False):
+    def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
-
-        self.in_channels = in_channels
-        self.out_channels = out_channels
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
             self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=kernel_size, stride=stride, padding=padding)
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
         # input is CHW
-        diff_y = x2.size()[2] - x1.size()[2]
-        diff_x = x2.size()[3] - x1.size()[3]
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
-                        diff_y // 2, diff_y - diff_y // 2])
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
-        return nn.Conv2d(x.size()[1], self.out_channels,  kernel_size=(1, 1))(x)
+        return self.conv(x)
 
 
 class OutConv(nn.Module):
